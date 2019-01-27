be272f040050c8646439579724d4f6b47ccfe786
Madforre <madforre@gmail.com>
2019-01-27 18:07:33 +0900

    webpack review

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+// ./src/index.js 기본 엔트리 설정 대신 설정된 script.js 파일이다.
+
+// 마치 JS 파일을 import 하듯이 CSS 파일을 import 한다.
+// 여기서 주의 사항은 반드시 style.css 앞에 ./ 를 붙여줘야 한다는 것이다.
+// 그렇지 않으면 프로젝트 최상위 디렉토리가 아닌 node_modules 디렉토리를 뒤지게 된다. 
+import './style.css'
+
 const message = document.createTextNode("Webpack Example")
 document.body.appendChild(message)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+// 설정 파일에 설치한 plugin을 import 하였다.
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
-    // Entry 설정의 기본값은 ./src/index.js이다. 아래와 같이 변경도 가능.
+    // Entry(들어감, 입구) 설정의 기본값은 ./src/index.js이다. 아래와 같이 변경도 가능.
     entry: './script.js',
     // 번들링 결과를 dist/main.js 기본 설정 대신 경로를 변경
     output: {
@@ -7,5 +10,25 @@ module.exports = {
         path: __dirname,
         // 이제 웹팩을 실행하면 최상위 디렉토리에 build.js 가 생성된다.
         filename: 'build.js'
-    }
+    },
+    // module 관련 속성 추가.
+    module: {
+        // rules 속성을 통해 처리 규칙을 등록
+        rules: [
+            {
+                // test 항목에 정의된 정규식과 매칭되는 파일은 
+                // use 항목에 등록된 로더를 통해서 처리되게 된다.
+                test: /\.css$/, // 확장자가 css 인 파일만 인식하도록 설정
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // plugins 속성에 import한 plugin을 추가하였다.
+        // 이제 웹팩을 실행하면 build.js 파일이 지워졌다가 다시 생성될 것이다!
+        new CleanWebpackPlugin('build.js'),
+    ]
 };
